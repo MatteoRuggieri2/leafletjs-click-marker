d961f763e0cb5e5f4b98dbc7e1f4091784597d08
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-02-25 01:45:04 +0100

    Coordinates displayed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// DOM Elements
+const latDomElement = document.getElementById('lat-value');
+const lngDomElement = document.getElementById('lng-value');
+
 // Italy Center
 const itaCenterLat = 42.6824;
 const itaCenterLng = 12.7880;
@@ -44,4 +48,15 @@ function onMapClick(e) {
 
     // Metto il nuovo marker
     marker = L.marker([clickedLat, clickedLng]).addTo(map);
+
+    console.log('lat: ', clickedLat);
+    console.log('lng: ', clickedLng);
+    console.log(latDomElement);
+    console.log(lngDomElement);
+
+    // Scrivo LAT
+    latDomElement.innerText = ` ${clickedLat}`;
+
+    // Scrivo LNG
+    lngDomElement.innerText = ` ${clickedLng}`;
 }
